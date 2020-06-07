c575459959ea453514ec6dc3ecd60f0ace8f14be
pavanIITB <kalepavan1234@gmail.com>
2020-06-07 17:06:22 +0530

    initialised the repo

---
+++ import.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO flights (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                   {"isbn": isbn, "title": title, "author": author, "year": year})
+        print(f"Added book \t ISBN number: {isbn} \t Title: {title} \t Author: {author} \t Year: {year}")
+    db.commit()
+
+if __name__ == "__main__":
+    main()
