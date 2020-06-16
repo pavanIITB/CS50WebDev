331d4e72d30a5313768a3f21b3ac4a1dbd53d86e
pavanIITB <kalepavan1234@gmail.com>
2020-06-16 20:12:31 +0530

    username stored in localstorage

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,7 +10,10 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
